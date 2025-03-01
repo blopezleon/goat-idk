a5b99dce4ed4c6f6e2b8113478201e62c5f01573
blopezleon <bernytenis05@gmail.com>
2025-03-01 18:23:45 -0500

    fkdlafjad

--- server/server.js
+++ server/server.js
@@ -15,10 +15,7 @@ const app = express();
 
 // More flexible CORS configuration
 app.use(cors({
-    // Allow all origins in development
-    origin: process.env.NODE_ENV === 'production' 
-        ? process.env.ALLOWED_ORIGINS?.split(',') 
-        : '*',
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
     methods: ['GET', 'POST'],
     credentials: true
 }));
